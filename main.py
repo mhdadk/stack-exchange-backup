f6b33a46bf04c54e075eeac03e572537588c501d
Mahmoud Abdelkhalek <maabdelk@ncsu.edu>
2024-01-14 19:16:43 +0400

    plan

--- main.py
+++ main.py
@@ -1,24 +1,63 @@
 """
 Given a network user id <user_id>, this script downloads all questions and answers for
-the corresponding user in the following format:
+the corresponding user into the following directories:
 questions
 |--- stack exchange site 1
-|---|--- question 1
-|---|--- question 2
+|---|--- question_1.md
+|---|--- question_2.md
 |---|--- ...
 |--- stack exchange site 2
-|---|--- question 1
-|---|--- question 2
+|---|--- question_1.md
+|---|--- question_2.md
 |---|--- ...
 answers
 |--- stack exchange site 1
-|---|--- answer 1
-|---|--- answer 2
+|---|--- answer_1.md
+|---|--- answer_2.md
 |---|--- ...
 |--- stack exchange site 2
-|---|--- answer 1
-|---|--- answer 2
+|---|--- answer_1.md
+|---|--- answer_2.md
 |---|--- ...
+
+To do this, this script proceeds as follows:
+1. Create a filter using the "/filters/create" method to get the network_user.account_id
+field from the network_users object returned by the "/users/{ids}/associated" method.
+2. Get all "network_user.account_id"'s for all stack exchange sites associated with
+the <user_id> using the "/users/{ids}/associated" method.
+3. For questions, create a filter using the "/filters/create" method to get the
+following fields from the "question" object type
+(https://api.stackexchange.com/docs/types/question):
+- answers
+- body_markdown
+- comments
+- creation_date
+- down_vote_count
+- up_vote_count
+- score
+4. For answers, create a filter using the "/filters/create" method to get the
+following fields from the "answer" object type
+(https://api.stackexchange.com/docs/types/answer):
+- question_id (go to the question and download it with the answers)
+TODO: remove these
+- accepted
+- body_markdown
+- comments
+- creation_date
+- is_accepted
+- down_vote_count
+- up_vote_count
+- score
+- question_id
+5. For each site extracted from step 2, do the following:
+    a. For each question extracted from this site, do the following:
+        i. Extract the fields mentioned in step 3 above using the created filter.
+        ii. Open a .md file and put the question, along with the answers, into the
+        file.
+    b. For each answer extracted from this site, do the following:
+        i. Extract the question_id field using the filter created in step 4.
+        ii. Open a .md file and put the question, along with the extracted answer, into
+        the file.
 """
 
 import requests
